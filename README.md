6bf0fe85e8800a2aed0907c2e5bc88c51ddbe26e
MayarSayed <36441485+MayarSayed@users.noreply.github.com>
2020-12-22 07:26:18 +0200

    Update readme file

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # Types of Horses
 
-## Table of Contents
+## Description
 
-* Overview
-* Horse types
-* An Image of the Horse type
-* Information about each type
+Building a multi-section landing page and dynamically building the navigation menu containing sections of the page
+Each the section actively being viewed is differentiated in color. and when a user clicks on a navigation item, the item scroll you to the appropriate section smoothly. 
 
 ## Overview
 There are more than 350 breeds of horses and ponies. From working and racing to casual riding and equestrian competition, each horse has its special qualities. But there are five particular breeds and five general horse categories that stand out and capture the hearts of horse lovers everywhere, thanks to their versatile abilities and good demeanors. Here are 4 of the most popular horses. 
 
-## 4 Horses types
+## Functionality
 
--American Quarter Horse<br />
--Arabian Horse<br />
--Thoroughbred Horse<br />
--Draft Breeds<br />
+* Build HTML with 4 content sections
+* Build the navigation menu. This will dynamically create a navigation menu based on the sections of the page.
+* Add functionality to distinguish the section in view. While navigating through the page, the section that is active in the viewport/closest to the top should be distinguished from the other sections.
+* Add the functionality to scroll to sections. Clicking on a navigation item should scroll to the appropriate section of the page.
+
+## Built With
+* CSS
+* HTML
+* JavaScript
--- js/app.js
+++ js/app.js
@@ -17,9 +17,9 @@
  * 
 */
 //stores list of sections on webpage
-var sectionsList = document.querySelectorAll('section');
+const sectionsList = document.querySelectorAll('section');
 //stores the navbar
-var narbarList = document.getElementById('navbar__list');
+const narbarList = document.getElementById('navbar__list');
 /**
  * End Global Variables
 
@@ -27,27 +27,25 @@ var narbarList = document.getElementById('navbar__list');
  * 
 */
 //building a document fragment and adding the navbar list items to it
-var fragmentNav = document.createDocumentFragment();
+const fragmentNav = document.createDocumentFragment();
 
+/*apply for each on all sections list and create a link then add it to the fragment list
+and add an event listener to the link on click to scroll to clicked section*/
 sectionsList.forEach((sec) => {
 
- 	var linkText = sec.getAttribute("data-nav");
- 	var secId = sec.getAttribute("id");
- 	var listItem = document.createElement("LI");
+ 	let linkText = sec.getAttribute("data-nav");
+ 	let secId = sec.getAttribute("id");
+ 	let listItem = document.createElement("LI");
 
- 	var secLink = document.createElement("a");
- 	var link = document.createTextNode(linkText);
+ 	let secLink = document.createElement("a");
+ 	let link = document.createTextNode(linkText);
     // Scroll to section on link click
     secLink.addEventListener('click', function () {
-    // Scroll to anchor ID using scrollTO event
     sec.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 	});
 
-    // Append the text node to anchor element. 
     secLink.appendChild(link);
-    // Append anchor element to list Item
     listItem.appendChild(secLink);
-    // Append the list Item to the document fragment
     fragmentNav.appendChild(listItem);
 });
 // build the nav by adding all sections anchors added to the document fragement to the navbar
@@ -63,20 +61,19 @@ narbarList.appendChild(fragmentNav);
 // Add class 'active' to section when near top of viewport
 document.addEventListener('scroll', function(){
 	sectionsList.forEach((sec) => {
-	var rect = sec.getBoundingClientRect();
+	let rect = sec.getBoundingClientRect();
 	if (rect.top >= 0 && rect.top <= 150){
 		sectionsList.forEach((secprev) => {secprev.classList.remove("your-active-class");});
 		// Set section as active
 		sec.classList.add("your-active-class");
 
-		var linksListItems = document.querySelectorAll('a');
+		const linksListItems = document.querySelectorAll('a');
 		//search for link with data nav equals the section
 		linksListItems.forEach((linkItem) => {
 
-			var linkText = linkItem.innerHTML;
+			let linkText = linkItem.innerHTML;
 			if(linkText == sec.getAttribute("data-nav")){
 				linksListItems.forEach((linkprev) => {linkprev.classList.remove("your-active-link");});
-				// set the link as active
 				linkItem.classList.add("your-active-link");
 			}
 		});
